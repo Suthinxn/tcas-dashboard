d8a308821c27816b537a2cb411d168172620b711
Suthinan Rongphon <suthinan.rpn@gmail.com>
2025-07-24 01:48:08 +0700

    ✨ feat: add scatter map

--- app/main.py
+++ app/main.py
@@ -1,19 +1,68 @@
 # Import packages
 from dash import Dash, html, dash_table, dcc
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 # Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+data = pd.read_csv("data/filtered_ai_com_courses.csv")
 
 # Initialize the app
 app = Dash()
 
+# Create scatter map figure
+def create_scatter_map(df):
+    # สร้าง scatter map
+    fig = go.Figure(go.Scattermap(
+        lat=df['latitude'],  # แทนที่ด้วยชื่อคอลัมน์ latitude ของคุณ
+        lon=df['longitude'],  # แทนที่ด้วยชื่อคอลัมน์ longitude ของคุณ
+        mode='markers',
+        marker=go.scattermap.Marker(
+            size=12,
+            color='red',
+            opacity=0.7
+        ),
+        text=df['university_name_th'],  # แทนที่ด้วยชื่อคอลัมน์ชื่อมหาวิทยาลัยของคุณ
+        hovertemplate='<b>%{text}</b><br>' +
+                      'Latitude: %{lat}<br>' +
+                      'Longitude: %{lon}<br>' +
+                      '<extra></extra>'
+    ))
+
+    # ตั้งค่า layout สำหรับประเทศไทย
+    fig.update_layout(
+        autosize=True,
+        hovermode='closest',
+        map=dict(
+            bearing=0,
+            center=dict(
+                lat=13.7563,  # กรุงเทพฯ
+                lon=100.5018  # กรุงเทพฯ
+            ),
+            pitch=0,
+            zoom=5.5,  # ซูมให้เห็นประเทศไทยทั้งหมด
+            style='open-street-map'  # ใช้ OpenStreetMap
+        ),
+        height=600,
+        margin=dict(r=0, t=0, l=0, b=0)
+    )
+    
+    return fig
+
 # App layout
 app.layout = [
-    html.Div(children='My First App with Data and a Graph'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
+    html.Div([
+        html.Div([
+            html.H1("TCAS-DASHBOARD", style={"text-align": "center"})
+        ]),
+        html.Div([
+            html.H2("Universities Location Map", style={"text-align": "center"}),
+            dcc.Graph(figure=create_scatter_map(data))
+        ]),
+        # html.Div([
+        #     html.H2("University Acceptance Data"),
+        #     dcc.Graph(figure=px.histogram(data, x='university_name_th', y='number_acceptance_mko2'))
+        # ])
+    ])
 ]
 
 # Run the app
